3484a358a75eb4992d54405f37f096a7852953d1
Manju Dn <manjudn016@gmail.com>
2020-03-20 15:24:57 +0530

    Falabella code challenge
    Falabella code challenge

---
+++ src/Calculation.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace manju\PHP;
+
+
+class Calculation
+{
+    function calcRemainder($number,$param )
+    {
+        return $number % $param;
+
+    }
+
+
+    function addRemainder($number3,$number5)
+    {
+        return $number3 +  $number5;
+    }
+
+}
